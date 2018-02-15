bfb655a5c67eab50525d92f24cf77b4c62e1d9d1
pellmell <gameyanagi@ezweb.ne.jp>
2018-02-16 00:18:08 +0900

    Update main.js

--- tween_test/main.js
+++ tween_test/main.js
@@ -1,32 +1,61 @@
-var canvas = document.getElementById('canvas');
+var canvas;
 
 var sprite;
 var stage;
 var renderer;
 var stats;
 //定義したTimelineに対して一時停止、再生、逆再生をかける。
-var tl;
-var rev = false;
 
+var log;
+
+var posX,posY;
 (function initLoad()
 {
+	canvas = document.getElementById('canvas');
 	var ua = navigator.userAgent;
     if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
         canvas.addEventListener('touchstart',function(e){
-		clickact();
+		clickact(e);
 	},false);
+	canvas.addEventListener('touchmove', function(e) {
+		var mouseX = e.pageX;
+		var mouseY = e.pageY;
+		clickmove(mouseX,mouseY);
+		},false);
     }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
         canvas.addEventListener('touchstart',function(e){
-		clickact();
+		clickact(e);
 	},false);
+	canvas.addEventListener('touchmove', function(e) {
+		var mouseX = e.pageX;
+		var mouseY = e.pageY;
+
+		},false);
     }else{
-	window.onmousedown = function( event ) {
-		clickact();
+	canvas.onmousedown = function( e ) {
+		clickact(e);
+	};
+	canvas.onmousemove = function( e ) {
+		var mouseX = e.pageX;
+		var mouseY = e.pageY;
+		clickmove(mouseX,mouseY);
 	};
     }
+	
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	document.getElementById("list").innerHTML = "width:"+canvas.width + " height:" +canvas.height;
+	
+	document.getElementById("time").value = 5;
+	document.getElementById("rotation").value = 0;
+	document.getElementById("repeat").value = 0;
+	document.getElementById("delay").value = 0;
+	
+	log = document.getElementById("logging");
+
+	posX = document.getElementById("posX");
+	posY = document.getElementById("posY");
+	
+	writeLog("width:"+canvas.width + " height:" +canvas.height);
 	
 	stage = new PIXI.Container();
         renderer = PIXI.autoDetectRenderer(
@@ -34,9 +63,13 @@ var rev = false;
         canvas.height,
         {view: canvas,antialiasing: true},false,true
     );
-	tl = new TimelineMax();
+
 	Init();
 })();
+function writeLog(logtext)
+{
+	log.innerHTML += logtext + "<br>";
+}
 function chack_functions(source)
 {
 	var propNames = [];
@@ -58,7 +91,7 @@ var repeat;
 var ease;
 var delay;
 var time;
-var onComplete;/*関数*/
+
 function Add()
 {
 	var anim = {
@@ -70,25 +103,36 @@ function Add()
 	delay:delay?delay:undefined,
 	rotation: rotation?rotation:undefined,//3.14*2.0
     repeat: repeat?repeat:undefined, // 繰り返し回数指定 -1はループ
-    ease: ease?ease:undefined,// 加減速の種類
-	onComplete : onComplete?function(){}:undefined
+    ease: ease?ease:undefined// 加減速の種類
 	};
-	var animlist = 
-	x?x:undefined + ":" + 
-	y?y:undefined + ":" + 
-	tint?tint:undefined + ":" + 
-	width?width:undefined + ":" + 
-	height?height:undefined + ":" + 
-	delay?delay:undefined + ":" + 
-	rotation?rotation:undefined + ":" + 
-	repeat?repeat:undefined + ":" + 
-	ease?ease:undefined + ":" + 
-	onComplete + "<br>";
-	document.getElementById("list").innerHTML += animlist;
-	
+
+	var list = document.getElementById("list");
+	if(time == undefined){
+		list.innerHTML = "時間を設定してください";
+		return;
+	}
+	list.innerHTML = '';
 	anims.push(anim);
 	times.push(time);
+
+	for(var i = 0;i < anims.length;i++)
+	{
+		var animls = anims[i];
+		var astr = 
+		times[i]?times[i]:undefined + ":" + 
+		animls.x?animls.x:undefined + ":" + 
+		animls.y?animls.y:undefined + ":" + 
+		animls.tint?animls.tint:undefined + ":" + 
+		animls.width?animls.width:undefined + ":" + 
+		animls.height?animls.height:undefined + ":" + 
+		animls.delay?animls.delay:undefined + ":" + 
+		animls.rotation?animls.rotation:undefined + ":" + 
+		animls.repeat?animls.repeat:undefined + ":" + 
+		animls.ease?animls.ease:undefined + ":" + "<br>";
 	
+		list.innerHTML += astr;
+	}
+	time = undefined;
 	x = undefined;
 	y= undefined;
 	tint= undefined;
@@ -98,17 +142,32 @@ function Add()
 	repeat= undefined;
 	ease= undefined;
 	delay= undefined;
-	onComplete= undefined;/*関数*/
 }
 function Remove()
 {
 	anims.pop();
 	times.pop();
+	document.getElementById("list").innerHTML += "アニメーションを戻します";
 }
 function allRemove()
 {
-	anims.clear();
-	times.clear();
+	anims.length = 0;
+	times.length = 0;
+	document.getElementById("list").innerHTML = "アニメーションを全消去します";
+}
+function Clear()
+{
+	sprite.x = 0;
+	sprite.y = 0;
+	sprite.tint = rgb(255,255,255);
+	sprite.width = canvas.width;
+	sprite.height = canvas.height;
+	sprite.rotation = 0;
+	posX.value = 0;
+	posY.value = 0;
+	document.getElementById("rotation").value = 0;
+	document.getElementById("width").value = sprite.width;
+	document.getElementById("height").value = sprite.height;
 }
 function timeSet()
 {
@@ -118,8 +177,8 @@ function timeSet()
 }
 function posSet()
 {
-	var xl = document.getElementById("posX").value;
-	var yl = document.getElementById("posY").value;
+	var xl = posX.value;
+	var yl = posY.value;
 	if(xl && yl)
 	{
 		sprite.x = xl;
@@ -130,27 +189,33 @@ function posSet()
 }
 function colorSet()
 {
-	var rl = document.getElementById("colorR").value;
-	var gl = document.getElementById("colorG").value;
-	var bl = document.getElementById("colorB").value;
-	if(rl && gl && bl){
-	tint = rgb(rl,gl,bl);
+	var col = $('.jscolor').val();
+	if(col){
+	var parse_col_val = parseInt(col, 16);//16進数文字列を10進数整数に変換
+	tint = parse_col_val;
+	sprite.tint = tint;
 	}
 }
 function sizeSet()
 {
 	var widthl = document.getElementById("width").value;
 	var heightl = document.getElementById("height").value;
-	if(widthl)
+	if(widthl){
 	width = widthl;
-	if(heightl)
+	sprite.width = width;
+	}
+	if(heightl){
 	height = heightl;
+	sprite.height = height;
+	}
 }
 function rotSet()
 {
 	var rotationl = document.getElementById("rotation").value;
-	if(rotationl)
+	if(rotationl){
 	rotation = ToRadians(rotationl);
+	sprite.rotation = rotation;
+	}
 }
 function repeatSet()
 {
@@ -193,11 +258,21 @@ function PlayAnim()
 	
 	for(var i = 0;i < anims.length;i++)
 	{
-		line.to(sprite,times[i],anims[i]);
+		line.to(sprite,times[i]/*実行秒*/,anims[i]/*アニメ*/);
 	}
 	var pl = $('input[name=_radio]:checked').val();
-	if(pl == 'play') line.Play();
-	else line.reverse();
+	if(pl == 'play') {
+		line.eventCallback("onComplete",function(){
+		writeLog('onComplete');
+	});
+		line.Play();
+	}
+	else {
+		line.eventCallback("onReverseComplete",function(){
+		writeLog('onReverseComplete');
+	});
+		line.reverse();
+	}
 }
 function ToRadians(fDegrees) 
 { 
@@ -218,14 +293,6 @@ function setSprite_State()
 	repeatSet();
 	delaySet();
 	easeSet();
-	
-	var listhtml = document.getElementById("list");
-	listhtml.innerHTML = "width:"+canvas.width + " height:" +canvas.height;
-	
-	for(var i = 0;i < anims.length;i++)
-	{
-		listhtml.innerHTML += "time:"+times[i] + " anims:" + anims[i] + "<br>";
-	}
 }
 function Init()
 {
@@ -236,11 +303,15 @@ function Init()
     sprite = new PIXI.Sprite(farTex);
 	sprite.width = canvas.width;
 	sprite.height = canvas.height;
+	posX.value = 0;
+	posY.value = 0;
+	document.getElementById("width").value = sprite.width;
+	document.getElementById("height").value = sprite.height;
 
-	var anim1 = {
+	/*var anim1 = {
 		x: 0,
 		y: 0,
-		tint:rgb(255,255,0),/*色*/
+		tint:rgb(255,255,0),
 		width: canvas.width * 0.5,
 		height: canvas.height * 0.5,
 		rotation: ToRadians(360),
@@ -255,19 +326,22 @@ function Init()
 		height: canvas.height * 0.5,
 		rotation: 0,
 		delay:1,
-        repeat: 0,
+        repeat: 0
        // ease: Cubic.easeInOut,
-		onComplete : function(){
-		alert('comp');
+		/*onComplete : function(){
+		writeLog('comp');
 		} // 処理完了後に呼ばれる関数を指定できます。
 	};
-	tl.to(sprite,3.0/*秒指定*/,anim1);
-	tl.to(sprite,2.0,/*秒指定*/anim2);
-	tl.to(sprite,1.0/*秒指定*/,anim1);
+	tl.to(sprite,3.0,anim1);
+	tl.to(sprite,2.0,anim2);
+	tl.to(sprite,1.0,anim1);
+	
 
 	tl.pause();
-
+*/
     stage.addChild(sprite);
+
+		
 	});
 	
 	// 画面回転時に向きをチェック
@@ -277,18 +351,20 @@ function Init()
 	document.body.appendChild(stats.domElement);
     this.enterFrameHandler();
 }
-function Play()
+
+function clickact(e)
 {
-	if(rev){
-		tl.reverse();
-	}
-	else {
-		tl.play();
-	}
-	rev = !rev;
+	var mouseX = e.pageX*1.43;
+	var mouseY =  e.pageY*1.19;
+
+	sprite.x = mouseX;
+	sprite.y = mouseY;
+	posX.value = mouseX;
+	posY.value = mouseY;
 }
-function clickact()
+function clickmove(mouseX,mouseY)
 {
+	
 }
 function enterFrameHandler()
 {
@@ -296,10 +372,10 @@ function enterFrameHandler()
           this.enterFrameHandler()
         });
 		if(stats)stats.update();
-		if(tl.isActive())
+		/*if(tl.isActive())
 		{
 			
-		}
+		}*/
 		/*tl.progress()*/
     //Render the stage
     this.renderer.render(this.stage);
