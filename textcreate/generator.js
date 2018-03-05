f41bfc0370fb0d037ef6dc6b7ee6264b65179aa9
pellmell <gameyanagi@ezweb.ne.jp>
2018-03-05 15:41:18 +0900

    Add files via upload

---
+++ textcreate/generator.js
@@ -0,0 +1,277 @@
+(function(global) {
+	'use strict';
+	var m_call;
+	var canvas = document.createElement('canvas'),
+		buffer = document.createElement('canvas'),
+		ctxa = canvas.getContext('2d'),
+		ctxb = buffer.getContext('2d'),
+		ctx = ctxa;
+	canvas.width = 512;
+	canvas.height = 512;
+	buffer.width = canvas.width * 2;
+	buffer.height = canvas.height * 2;
+	var fontHeight = 40,
+		fontWidth = 26,
+		fontFamily = 'Arvo', //'Baumans',/*https://fonts.google.com/*/
+		//テキスト
+		text = '',
+		viewtext = '',
+		mono = false, //等幅
+		outline = true, //アウトライン
+		outlineOffsetX = 0,
+		outlineOffsetY = 0,
+		outlineWidth = 2.0,
+		shadow = false,
+		shadowColor = '#000000',
+		shadowBlur = 3.0,
+		shadowOffsetX = 0,
+		shadowOffsetY = 0,
+		textColor = '#FFFFFF',
+		outlineColor = '#849d19',
+		scale = 1.0,
+		ss = true,
+		bold = false,
+		italic = false,
+		grad = true,
+		endColor = '#FF7700',
+		grid = false,
+		baselineOffset = 0,
+		marginLeft = 0,
+		marginTop = 0,
+		background = false,
+		backgroundColor = '#000000';
+	var fontRects = [];
+
+	function setcall(call) {
+		m_call = call;
+	}
+	//png
+	function createFonturl() {
+		var imgUrl = canvas.toDataURL("image/png");
+		return imgUrl;
+	}
+	//json data
+	function createFontRects() {
+		var ff = {};
+		for (var i = 0; i < fontRects.length; i++) {
+			var f = {};
+			f.u = fontRects[i][1];
+			f.v = fontRects[i][2];
+			f.w = fontRects[i][3];
+			f.h = fontRects[i][4];
+			f.vx = fontRects[i][5];
+			f.vy = fontRects[i][6];
+			f.vw = fontRects[i][7];
+			f.vh = -fontRects[i][8];
+			ff[fontRects[i][0]] = f;
+		}
+		return ff;
+	}
+
+	function opt() {
+		var t = [];
+		for (var i = 0; i < text.length; i++) {
+			if (t.indexOf(text[i]) < 0) {
+				t.push(text[i]);
+			}
+		}
+		text = t.join('');
+	}
+
+	function loadFont(font) {
+		WebFont.load({
+			google: {
+				families: [font]
+			},
+			active: render,
+			inactive: function() {
+				console.log('inactive');
+				render();
+			}
+		});
+	}
+
+	function showAlpha() {
+		ctx = ctxa;
+		var image = ctx.getImageData(0, 0, canvas.width, canvas.height),
+			data = image.data;
+		for (var i = 0; i < data.length; i += 4) {
+			var a = data[i + 3];
+			data[i] = a;
+			data[i + 1] = a;
+			data[i + 2] = a;
+			data[i + 3] = 255;
+		}
+		ctx.putImageData(image, 0, 0);
+	}
+
+	function render() {
+		ctx = ss ? ctxb : ctxa;
+		ctx.save();
+		if (ss) {
+			ctx.scale(2.0, 2.0);
+		}
+		if (background) {
+			ctx.fillStyle = backgroundColor;
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+		} else {
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		}
+		var w = 0;
+		if (outline) {
+			w = outlineWidth;
+		}
+		var fontStyle = '';
+		fontStyle += italic ? 'italic ' : '',
+			fontStyle += bold ? 'bold ' : '',
+			fontStyle += fontHeight + 'px ',
+			fontStyle += fontFamily;
+		ctx.font = fontStyle;
+		ctx.strokeStyle = outlineColor;
+		ctx.lineWidth = w;
+		ctx.textBaseline = 'bottom';
+		ctx.textAlign = 'center';
+		ctx.lineCap = 'round';
+		ctx.lineJoin = 'round';
+		if (!mono) {
+			fontWidth = ctx.measureText(text[0]).width;
+		} else {
+			fontWidth = 0;
+			for (var j = 0; j < text.length; j++) {
+				var m = ctx.measureText(text[j]).width;
+				if (m > fontWidth) {
+					fontWidth = m;
+				}
+			}
+		}
+		ctx.fillStyle = textColor;
+		if (shadow) {
+			ctx.shadowBlur = shadowBlur;
+			ctx.shadowColor = shadowColor;
+			ctx.shadowOffsetX = shadowOffsetX;
+			ctx.shadowOffsetY = shadowOffsetY;
+			w += shadowBlur * 0.5;
+		}
+		var offsetX = w * 0.5 + fontWidth * 0.5,
+			offsetY = canvas.height - w * 0.5 - outlineOffsetY;
+		if (grad) {
+			var gradColor = ctx.createLinearGradient(0, offsetY - fontHeight, 0, offsetY);
+			gradColor.addColorStop(0, textColor);
+			gradColor.addColorStop(1, endColor);
+			ctx.fillStyle = gradColor;
+		}
+		var prevFontWidth = fontWidth;
+		var rect = [];
+		var col = 0,
+			row = 0;
+		fontRects.length = 0;
+		var fontH = fontHeight + marginTop;
+		for (var i = 0; i < text.length; i++) {
+			var c = text[i],
+				m = ctx.measureText(c);
+			if (!mono) {
+				offsetX -= fontWidth * 0.5;
+				offsetX += m.width * 0.5;
+				fontWidth = m.width;
+			}
+			if (offsetX + fontWidth * 0.5 + w + marginLeft > canvas.width) {
+				offsetY -= fontH + w + outlineOffsetY;
+				offsetX = w * 0.5 + fontWidth * 0.5;
+				if (grad) {
+					var gradColor = ctx.createLinearGradient(0, offsetY - fontH, 0, offsetY);
+					gradColor.addColorStop(0, textColor);
+					gradColor.addColorStop(1, endColor);
+					ctx.fillStyle = gradColor;
+				}
+				col = 0;
+				row++;
+			}
+			offsetX += marginLeft;
+			var size = {
+				x: 0,
+				y: 0,
+				w: fontWidth + w,
+				h: -(fontH + w)
+			};
+			var uv = {
+				x: (offsetX - w * 0.5 - fontWidth * 0.5) / canvas.width,
+				y: (canvas.height - (offsetY + w * 0.5)) / canvas.height + 0.01,
+				w: size.w / canvas.width,
+				h: (fontH + w) / canvas.height
+			};
+			if (grid) {
+				ctx.save();
+				ctx.fillStyle = (row & 1) ^ (col & 1) ? '#0F0' : '#F0F';
+				ctx.fillRect(offsetX - size.w / 2, offsetY, size.w, size.h);
+				ctx.restore();
+			}
+			if (outline) {
+				ctx.strokeText(c, offsetX + outlineOffsetX, offsetY + outlineOffsetY - baselineOffset);
+			}
+			ctx.fillText(c, offsetX, offsetY - baselineOffset);
+			col++;
+			var r = [
+				c.charCodeAt(), uv.x, uv.y, uv.w, uv.h,
+				size.x, size.y, size.w, size.h
+			];
+			rect.push(r.join(','));
+			fontRects.push(r);
+			offsetX += fontWidth + w;
+		}
+		ctx.restore();
+		if (ss) {
+			var ctx2 = canvas.getContext('2d');
+			ctx2.clearRect(0, 0, canvas.width, canvas.height);
+			ctx2.drawImage(buffer, 0, 0, canvas.width, canvas.height);
+		}
+		if (m_call) m_call();
+	}
+
+	function init() {
+		// WebGLコンテキストを設定
+		var canvas__ = document.getElementById('canvas');
+		canvas__.width = window.innerWidth;
+		canvas__.height = window.innerHeight;
+		var gl = canvas__.getContext('webgl') || canvas__.getContext('experimental-webgl');
+		var stats = new Stats();
+		stats.setMode(0);
+		document.body.appendChild(stats.domElement);
+		// WebGLコンテキストを設定
+		Font.init(gl);
+		var font = new Font(512); //引数:最大表示文字数(def:256)
+		// テクスチャを読み込み
+		var img = new Image();
+		img.onload = function() {
+			font.setTexture(img);
+			font.font = createFontRects();
+			glrender();
+		};
+		img.src = createFonturl();
+		var count = 0;
+		// テキスト描画
+		function glrender() {
+			requestAnimationFrame(glrender);
+			if (stats) stats.update();
+			gl.clearColor(0.0, 0.5, 0.5, 1.0);
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+			font.textAlign = 'left';
+			count++;
+			if (count <= viewtext.length) {
+				// テキストフィールドにデータを渡す処理
+				font.drawText(viewtext.substring(0, count), -canvas__.width / 2, canvas__.height / 2 - 42);
+			} else count = 0;
+			font.draw();
+			gl.flush();
+		}
+	}
+	//コンストラクタ
+	var fontGenerator = function(t) {
+		setcall(init);
+		text = t;
+		viewtext = text;
+		opt();
+		showAlpha();
+		loadFont(fontFamily);
+	};
+	global.fontGenerator = fontGenerator;
+})(this);
---
+++ textcreate/textutil.js
@@ -0,0 +1,257 @@
+(function(global) {
+	'use strict';
+	
+	var mesh = {};
+	
+	var position = [],
+		uv = [],
+		uniforms = [],
+		attributes = [],
+		program;
+	
+	var gl;
+
+	// default shader
+	var defualtFS =
+		"precision highp float;\n" +
+		"uniform sampler2D sampler;\n" +
+		"varying vec2 v_uv;\n" +
+		"varying vec4 v_color;\n" +
+		"void main() {\n" +
+		"vec4 tex = texture2D(sampler, v_uv);\n" +
+		"gl_FragColor = tex * v_color;\n" +
+		"}";
+	
+	var defaultVS =
+		"attribute vec3 position;\n" +
+		"attribute vec2 uv;\n" +
+		"attribute vec4 color;\n" +
+		"uniform vec2 screen;\n" +
+		"varying vec2 v_uv;\n" +
+		"varying vec4 v_color;\n" +
+		"void main() {" +
+		"v_uv = uv;\n" +
+		"v_color = max(color, 0.0);\n" +
+		"gl_Position = vec4(position.xy / screen.xy, 0.0, 1.0);\n" +
+		"}";
+	
+	function getShader(src, kind) {
+		var s = gl.createShader(kind);
+		gl.shaderSource(s, src);
+		gl.compileShader(s);
+//		console.log(gl.getShaderInfoLog(s));
+		return s;
+	}
+	
+	var Font = function(max) {
+		if(!max)max=256;
+		this.create(max);
+	};
+	
+	Font.init = function(context) {
+		gl = context;
+		var vs = getShader(defaultVS, gl.VERTEX_SHADER),
+			fs = getShader(defualtFS, gl.FRAGMENT_SHADER);
+		program = gl.createProgram();
+		gl.attachShader(program, vs);
+		gl.attachShader(program, fs);
+		gl.linkProgram(program);
+	};
+	
+	
+	Font.prototype.create = function(max) {
+		var buffer = new Float32Array(max * 9 * 4),
+			indexStream = new Uint16Array(max * 6),
+			k = 0;
+		for(var i = 0; i < max * 6; i += 6) {
+			indexStream[i + 0] = k + 0;
+			indexStream[i + 1] = k + 1;
+			indexStream[i + 2] = k + 2;
+			indexStream[i + 3] = k + 0;
+			indexStream[i + 4] = k + 2;
+			indexStream[i + 5] = k + 3;
+			k += 4;
+		}
+		var vbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+		gl.bufferData(gl.ARRAY_BUFFER, buffer, gl.DYNAMIC_DRAW);
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+		
+		var ibo = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexStream, gl.STATIC_DRAW);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+		
+		this.uniforms = [];
+		this.uniforms[0] = gl.getUniformLocation(program, 'sampler');
+		this.uniforms[1] = gl.getUniformLocation(program, 'screen');
+		this.attributes = [];
+		this.attributes[0] = gl.getAttribLocation(program, 'position');
+		this.attributes[1] = gl.getAttribLocation(program, 'uv');
+		this.attributes[2] = gl.getAttribLocation(program, 'color');
+		
+		this.indexStream = indexStream;
+		this.buffer = buffer;
+
+		this.ibo = ibo;
+		this.vbo = vbo;
+		this.context = gl;
+		this.offset = 0;
+		this.count = 0;
+		this.dirty = true;
+		this.size = [1, 1];
+		this.textAlign = 'left';
+		this.font = null;
+		this.texture = null;
+	};
+	
+	Font.prototype.shader = function(p) {
+		if(p) {
+			
+		}
+	};
+	
+	Font.prototype.setTexture = function(image) {
+		if(this.texture) gl.deleteTexture(this.texture);
+		this.texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+		gl.generateMipmap(gl.TEXTURE_2D);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	};
+	
+	Font.loadFont = function(name, cb) {
+	};
+	Font.loadTexture = function(name, cb) {
+	}
+	Font.prototype.getCharacter = function(code) {
+		return this.font && (code in this.font) ? this.font[code] : null;
+	};
+	
+	Font.prototype.getTextWidth = function(text) {
+		var w = 0;
+		for(var i = 0; i < text.length; i++) {
+			var t = text[i],
+				uv = this.getCharacter(t.charCodeAt());
+			if(uv) {
+				w += uv.vw;
+			}
+		}
+		return w;
+	};
+	
+	Font.prototype.setSize = function(x, y) {
+		this.size[0] = x;
+		this.size[1] = y;
+	}
+	
+	Font.prototype.textAlign = function(a) {
+		this.textAlign = a;
+	}
+	Font.prototype.getBind = function() {
+		return this.bind;
+	}
+	
+	
+	/**
+	 * draw text
+	 */
+	Font.prototype.drawText = function(text, x, y, color) {
+
+		if(!this.dirty) {
+			this.count = 0;
+			this.offset = 0;
+		}
+		if(typeof text !== 'string') {
+			text = '' + text;
+		}
+		color = color || [1, 1, 1, 1];
+		var offsetX = 0,
+			offsetY = 0;
+		
+		if(this.textAlign === 'center') {
+			offsetX -= this.getTextWidth(text) * this.size[0] * 0.5;
+		}
+		var offset = this.offset,
+			buffer = this.buffer;
+		for(var i = 0; i < text.length; i++) {
+			if(typeof text[i] === 'string') {
+				var uv = this.getCharacter(text[i].charCodeAt());
+				
+				if(uv) {
+					var vw = uv.vw * this.size[0],
+						vh = uv.vh * this.size[1],
+						vx = [x, x, x + vw, x + vw],
+						vy = [y + vh, y, y, y + vh],
+						u = [uv.u, uv.u, uv.u + uv.w, uv.u + uv.w],
+						v = [uv.v + uv.h, uv.v, uv.v, uv.v + uv.h];
+					
+					if(text[i] === '\n') {
+						offsetY -= uv.vh;
+						if(this.textAlign === 'center') {
+						offsetX = -(this.getTextWidth(text) * this.size[0] * 0.5);
+						}else
+							offsetX = -uv.vw;
+					}
+					
+					for(var j = 0; j < 4; j++) {
+						buffer[offset + 0] = vx[j] + offsetX;
+						buffer[offset + 1] = vy[j] + offsetY;
+						buffer[offset + 2] = 0;
+						buffer[offset + 3] = u[j];
+						buffer[offset + 4] = v[j];
+						buffer[offset + 5] = color[0];
+						buffer[offset + 6] = color[1];
+						buffer[offset + 7] = color[2];
+						buffer[offset + 8] = color[3];
+						offset += 9;
+					}
+					offsetX += vw;
+				}
+			}
+		}
+		this.count += text.length;
+		this.offset = offset;
+		this.dirty = true;
+		return offsetX;
+	};
+	
+	/**
+	 * draw
+	 */
+	Font.prototype.draw = function() {
+		gl.enable(gl.BLEND);
+		gl.blendEquation(gl.FUNC_ADD);
+		gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA, gl.ZERO, gl.ONE);
+		
+		gl.useProgram(program);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ibo);
+		if(this.dirty) {
+			gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.buffer.subarray(0, this.offset));
+			this.dirty = false;
+		}
+		gl.activeTexture(gl.TEXTURE0);
+		gl.bindTexture(gl.TEXTURE_2D, this.texture);
+		gl.uniform1i(this.uniforms[0], 0);
+		var w = gl.canvas.width * 0.5,
+			h = gl.canvas.height * 0.5;
+		gl.uniform2f(this.uniforms[1], w, h);
+		gl.enableVertexAttribArray(this.attributes[0]);
+		gl.enableVertexAttribArray(this.attributes[1]);
+		gl.enableVertexAttribArray(this.attributes[2]);
+		var size = Float32Array.BYTES_PER_ELEMENT,
+			stride = 9 * size;
+		gl.vertexAttribPointer(this.attributes[0], 3, gl.FLOAT, false, stride, 0);
+		gl.vertexAttribPointer(this.attributes[1], 2, gl.FLOAT, false, stride, 3 * size);
+		gl.vertexAttribPointer(this.attributes[2], 4, gl.FLOAT, false, stride, 5 * size);
+		gl.drawElements(gl.TRIANGLES, this.count * 6, gl.UNSIGNED_SHORT, 0);
+	};
+	
+	global.Font = Font;
+})(this);
